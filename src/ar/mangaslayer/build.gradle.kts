b04e440843fa43fb5b344a510302bac9f0802e6b
aboibrahim3 <abooder504@gmail.com>
2026-01-18 00:28:40 +0300

    Create build.gradle.kts

---
+++ src/ar/mangaslayer/build.gradle.kts
@@ -0,0 +1,10 @@
+ext {
+    extName = 'MangaSlayer'
+    extClass = '.MangaSlayer'
+    themePkg = 'mangaslayer'
+    baseUrl = 'https://mangaslayer.com'
+    overrideVersionCode = 1
+    isNsfw = false
+}
+
+apply from: "$rootDir/common.gradle"
